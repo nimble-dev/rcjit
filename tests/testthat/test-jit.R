1ab405fce22e090aec062f52de6352826fdb08d4
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-06-22 13:38:06 -0700

    Fixes to pass R CMD check --as-cran

--- tests/testthat/test-jit.R
+++ tests/testthat/test-jit.R
@@ -4,7 +4,7 @@ nimTypeOfValues <- list(
     list('double(0)', 1.2),
     list('double(0)', c(1.2)),
     list('double(1)', c(1.2, 3.4)),
-    list('double(2)', matrix(double(1:4), 2, 2)),
+    list('double(2)', matrix(as.double(1:4), 2, 2)),
     list('integer(0)', 1L),
     list('integer(0)', c(1L)),
     list('integer(1)', c(2L, 1L)),
